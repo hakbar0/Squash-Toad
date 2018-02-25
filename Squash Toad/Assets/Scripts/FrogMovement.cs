6f0f14173d099ed00919bde2c11f234fbb44a158
hakbar0 <haseeb2000@hotmail.com>
2018-02-25 18:38:50 +0000

    Not able to double jump anymore.

--- Squash Toad/Assets/Scripts/FrogMovement.cs
+++ Squash Toad/Assets/Scripts/FrogMovement.cs
@@ -6,7 +6,8 @@ public class FrogMovement : MonoBehaviour {
 
 	public float jumpSpeedInMps = 5;
 	public float jumpElevationInDegrees = 45;
-
+	public float jumpGroundClearance = 2;
+	public float JumpSpeedTol = 5;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,19 +16,16 @@ public class FrogMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		var camera = GetComponentInChildren<Camera> ();
+		bool isOnGround = Physics.Raycast (transform.position, -transform.up, jumpGroundClearance);
 
-		Debug.DrawRay (transform.position, 	camera.transform.forward, Color.red);
-
-		var projectedLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
-		var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
-		var unnormalisedRotatedJumpDirection = Vector3.RotateTowards (projectedLookDirection, Vector3.up, radiansToRotate, 0 );
-		var jumpVector = unnormalisedRotatedJumpDirection.normalized * jumpSpeedInMps;
-		Debug.DrawRay (transform.position, jumpVector, Color.blue);
-
-
-		if (Input.GetKeyDown (KeyCode.Space)) {
-	
+		var speed = GetComponent<Rigidbody> ().velocity.magnitude;
+		bool isNearStationary = speed < JumpSpeedTol;
+		if (GvrViewer.Instance.Triggered && isOnGround && isNearStationary) {
+			var camera = GetComponentInChildren<Camera> ();
+			var projectedLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
+			var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+			var unnormalisedRotatedJumpDirection = Vector3.RotateTowards (projectedLookDirection, Vector3.up, radiansToRotate, 0 );
+			var jumpVector = unnormalisedRotatedJumpDirection.normalized * jumpSpeedInMps;
 			GetComponent<Rigidbody> ().AddForce (jumpVector, ForceMode.VelocityChange);
 		}
 	}
