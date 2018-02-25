fae1e509b968ca3fba8d0c54f121f8ceb8b67666
hakbar0 <haseeb2000@hotmail.com>
2018-02-25 17:17:04 +0000

    Vector Rotation.

--- Squash Toad/Assets/Scripts/FrogMovement.cs
+++ Squash Toad/Assets/Scripts/FrogMovement.cs
@@ -11,7 +11,20 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		var projectedJumpVector = Vector3.ProjectOnPlane (jumpVector, Vector3.up);
+
+		Debug.DrawRay (transform.position, projectedJumpVector, Color.blue);
+
+
+		var radiansToRotate = Mathf.Deg2Rad * 90;
+		var rotatedJumpVector = Vector3.RotateTowards (projectedJumpVector, Vector3.up, radiansToRotate, 0 );
+
+		Debug.DrawRay (transform.position, rotatedJumpVector.normalized, Color.blue);
+
+
 		if (Input.GetKeyDown (KeyCode.Space)) {
+	
 			GetComponent<Rigidbody> ().AddForce (jumpVector, ForceMode.VelocityChange);
 		}
 	}
