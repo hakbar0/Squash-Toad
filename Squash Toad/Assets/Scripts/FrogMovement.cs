c85e8e6f05a15ee278605a9896662112b7b9e320
hakbar0 <haseeb2000@hotmail.com>
2018-02-26 17:20:41 +0000

    Able to jump of the log.

--- Squash Toad/Assets/Scripts/FrogMovement.cs
+++ Squash Toad/Assets/Scripts/FrogMovement.cs
@@ -8,19 +8,27 @@ public class FrogMovement : MonoBehaviour {
 	public float jumpElevationInDegrees = 45;
 	public float jumpGroundClearance = 2;
 	public float JumpSpeedTol = 5;
+
+	public int collisionCount = 0;
 	// Use this for initialization
 	void Start () {
 		
+	}
+
+	void OnCollisionEnter(){
+		collisionCount++;
+	}
+
+	void OnCollisionExit(){
+		collisionCount--;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		bool isOnGround = Physics.Raycast (transform.position, -transform.up, jumpGroundClearance);
+		bool isOnGround = collisionCount > 0;
 
-		var speed = GetComponent<Rigidbody> ().velocity.magnitude;
-		bool isNearStationary = speed < JumpSpeedTol;
-		if (GvrViewer.Instance.Triggered && isOnGround && isNearStationary) {
+		if (GvrViewer.Instance.Triggered && isOnGround ) {
 			var camera = GetComponentInChildren<Camera> ();
 			var projectedLookDirection = Vector3.ProjectOnPlane (camera.transform.forward, Vector3.up);
 			var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
