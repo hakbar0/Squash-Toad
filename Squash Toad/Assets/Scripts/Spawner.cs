7f633e56340d94ce4f9879e303b22e3ce07345d6
hakbar0 <haseeb2000@hotmail.com>
2018-02-26 14:26:59 +0000

    Made random movement.

---
+++ Squash Toad/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject prefab;
+	public float spawnInterval = 1;
+	public float spawnIntervalMax = 4;
+	float nextSpawnTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawnTime) {
+			Spawn ();
+			nextSpawnTime = Time.time + Random.Range(spawnInterval, spawnIntervalMax);
+		}
+	}
+	void Spawn(){
+		var instance = Instantiate (prefab, transform.position, transform.rotation, transform);
+	}
+}
